60e3a14fb56175e60ed85c5c4b9a22f3c10c9e65
benday <benday@benday.com>
2021-11-08 17:03:16 -0500

    got tetss passing after updates

--- src/main.ts
+++ src/main.ts
@@ -83,7 +83,17 @@ async function run(): Promise<void> {
       pathToDirectory
     )
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      const err: Error = error
+
+      core.error(err)
+      core.setFailed(err)
+    } else {
+      core.error('Someting went wrong.')
+      core.error(JSON.stringify(error))
+      core.error(JSON.stringify(error))
+      core.setFailed(JSON.stringify(error))
+    }
   }
 }
 
